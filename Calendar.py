cdfc86181db63db2503350ab7f26d3c9520b631d
Mantaj Singh Sidhu <mantajsidhu1119@gmail.com>
2025-10-12 19:03:07 +0530

    Updated

--- Calendar.py
+++ Calendar.py
@@ -1,11 +1,12 @@
 import calendar
 
-year = int(input("Enter the year: "))
+print("\n"+"*"*20,"\033[1;31m"," Calendar ","\033[1;0m","*"*20,"\n")
 
-print("\n\n***** Calendar *****")
+# User Input
+year = int(input("Enter the year:\033[7;31m"))
 
+# Printing Every month calendar
 i = 1
-
 while i<=12:
    print("\n")
    calendar.TextCalendar(calendar.SUNDAY).prmonth(year,i)
